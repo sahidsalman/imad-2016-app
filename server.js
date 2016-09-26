3e5d22ae25c4e2d531f40e78f9d4fa2b3c767cf6
sahidsalman <sahid_b140118ec@nitc.ac.in>
2016-09-26 22:48:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article1', function (req, res){
+  res.send('career1');  
+});
+app.get('/article2', function (req, res){
+  res.send('career2');  
+});
+app.get('/article3', function (req, res){
+  res.send('career3');  
+});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
